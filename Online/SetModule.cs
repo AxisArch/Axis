9b48aeb7cd6a171a3c44b6c53264664ab6aef9f5
Povl Filip Sonne-Frederiksen <psf@aarch.dk>
2019-02-07 21:52:25 +0100

    Connect Component, updated sketch of SetModule Component, start of new AxisController Class

--- Online/SetModule.cs
+++ Online/SetModule.cs
@@ -8,6 +8,7 @@ using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
 using Grasshopper.Kernel.Parameters;
 
+
 using Rhino.Geometry;
 
 using ABB.Robotics.Controllers;
@@ -44,13 +45,9 @@ namespace Axis.Online
         NetworkScanner scanner = new NetworkScanner();
         ControllerInfo[] controllers = null;
 
-        ABB.Robotics.Controllers.RapidDomain.RobTarget cRobTarg;
-        System.Byte[] data;
 
         // Create a list of string to store a log of the connection status.
         private List<string> log = new List<string>();
-        private List<string> IOstatus = new List<string>();
-        private Plane tcp = new Plane();
 
 
         /// <summary>
@@ -68,16 +65,11 @@ namespace Axis.Online
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddBooleanParameter("Activate", "Activate", "Activate the online communication module.", GH_ParamAccess.item, false);
-            pManager.AddBooleanParameter("Scan", "Scan", "Scan the network for available controllers.", GH_ParamAccess.item, false);
-            pManager.AddBooleanParameter("Kill", "Kill", "Kill the process; logoff and dispose of network controllers.", GH_ParamAccess.item, false);
-            pManager.AddBooleanParameter("Clear", "Clear", "Clear the communication log.", GH_ParamAccess.item, false);
-            pManager.AddTextParameter("IP", "IP", "IP adress of the controller to connect to.", GH_ParamAccess.item);
-            pManager.AddBooleanParameter("Connect", "Connect", "Connect to the network controller.", GH_ParamAccess.item, false);
+            pManager.AddGenericParameter("Controller", "Controller", "Recives the output from a controller module", GH_ParamAccess.item);
             pManager.AddBooleanParameter("Send", "Send", "Send to module", GH_ParamAccess.item, false);
             pManager.AddTextParameter("Moduel", "Module", "Module to be wtritten to the controller.", GH_ParamAccess.list);
 
-            for (int i = 0; i < 7; i++)
+            for (int i = 0; i < 2; i++)
             {
                 pManager[i].Optional = true;
             }
@@ -97,158 +89,23 @@ namespace Axis.Online
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             bool activate = false;
-            bool scan = false;
-            bool kill = false;
-            bool clear = false;
-            string ip = System.String.Empty;
-            int index = 0;
-            bool connect = false;
-            bool resetPP = false;
-            bool begin = false;
 
-            if (!DA.GetData(0, ref activate)) ;
-            if (!DA.GetData(1, ref scan)) ;
-            if (!DA.GetData(2, ref kill)) ;
-            if (!DA.GetData(3, ref clear)) ;
-            if (!DA.GetData(4, ref ip)) ;
-            if (!DA.GetData(5, ref index)) ;
-            if (!DA.GetData(6, ref connect)) ;
-            if (!DA.GetData(7, ref resetPP)) ;
-            if (!DA.GetData(8, ref begin)) ;
+            if (!DA.GetData("Controller", ref activate)) ;
+            if (!DA.GetData("Send", ref activate)) ;
+            if (!DA.GetData("Moduel", ref activate)) ;
 
-
-            this.controllerIndex = index;
-            this.start = begin;
-
-            double cRobX = 0;
-            double cRobY = 0;
-            double cRobZ = 0;
-
-            double cRobQ1 = 0;
-            double cRobQ2 = 0;
-            double cRobQ3 = 0;
-            double cRobQ4 = 0;
-
-            Quaternion cRobQuat = new Quaternion();
 
             if (activate)
             {
-                if (scan)
+                if (logOption)
                 {
-                    // Scan the network for controllers and add them to our controller array
-                    scanner.Scan();
-                    controllers = scanner.GetControllers();
-
-                    if (controllers.Length > 0)
-                    {
-                        log.Add("Controllers found:");
-
-                        // List the controller names that were found on the network.
-                        for (int i = 0; i < controllers.Length; i++)
-                        {
-                            log.Add(controllers[i].ControllerName);
-                        }
-                    }
-                    else { log.Add("Scan timed out. No controllers were found."); }
+                    Status = log;
+                    DA.SetDataList("Log", log);
                 }
-
-                if (kill && controller != null)
-                {
-                    controller.Logoff();
-                    controller.Dispose();
-                    controller = null;
-
-                    log.Add("Process killed! Abandon ship!");
-                }
-
-                if (clear)
-                {
-                    log.Clear();
-                    log.Add("Log cleared.");
-                }
-
-                if (connect)
-                {
-                    if (controller == null && controllers.Length > 0)
-                    {
-                        string controllerID = controllers[index].ControllerName;
-                        log.Add("Selected robot controller: " + controllers[index].ControllerName + ".");
-
-                        if (controllers[index].Availability == Availability.Available)
-                        {
-                            log.Add("Robot controller " + controllers[index].ControllerName + " is available.");
-
-                            if (controller != null)
-                            {
-                                controller.Logoff();
-                                controller.Dispose();
-                                controller = null;
-                            }
-
-                            controller = ControllerFactory.CreateFrom(controllers[index]);
-                            controller.Logon(UserInfo.DefaultUser);
-                            log.Add("Connection to robot controller " + controller.SystemName + " established.");
-
-                            // Get T_ROB1 queue to send messages to the RAPID task.
-                            if (!controller.Ipc.Exists("RMQ_T_ROB1"))
-                            {
-                                controller.Ipc.CreateQueue("RMQ_T_ROB1", 10, Ipc.MaxMessageSize);
-                            }
-
-                            tasks = controller.Rapid.GetTasks();
-                            IpcQueue robotQueue = controller.Ipc.GetQueue("RMQ_T_ROB1");
-                            int queueID = robotQueue.QueueId;
-                            string queueName = robotQueue.QueueName;
-
-                            log.Add("Established RMQ for T_ROB1 on network controller.");
-                            log.Add("Rapid Message Queue ID:" + queueID.ToString() + ".");
-                            log.Add("Rapid Message Queue Name:" + queueName + ".");
-                            RobotQueue = robotQueue;
-                        }
-                        else
-                        {
-                            log.Add("Selected controller not available.");
-                        }
-
-                        ControllerID = controllerID;
-                    }
-                    else
-                    {
-                        if (controller != null)
-                        {
-                            return;
-                        }
-                        else
-                        {
-                            string exceptionMessage = "No robot controllers found on network.";
-                            ControllerID = "No Controller";
-                            log.Add(exceptionMessage);
-                        }
-                    }
-                }
-
-                if (resetPP && controller != null)
-                {
-                    using (Mastership m = Mastership.Request(controller.Rapid))
-                    {
-                        // Reset program pointer to main.
-                        tasks[0].ResetProgramPointer();
-                        log.Add("Program pointer set to main on the active task.");
-                    }
-                }
-
-                // To be changed in to optional logoutput
-                // Output the status of the connection.
-                /*Status = log;
-
-                DA.SetDataList(0, log);
-                DA.SetDataList(1, IOstatus);
-                DA.SetData(2, new GH_Plane(tcp));
-                */
 
                 ExpireSolution(true);
             }
-
+            
         }
         
 
@@ -264,7 +121,7 @@ namespace Axis.Online
         // Build a list of optional output parameters
         IGH_Param[] outputParams = new IGH_Param[1]
         {
-        new Param_String() { Name = "Log", NickName = "Log", Description = "Log checking the connection status" },
+        new Param_String() { Name = "Log", NickName = "Log", Description = "Log checking the connection status"},
         };
 
         // The following functions append menu items and then handle the item clicked event.
@@ -277,10 +134,21 @@ namespace Axis.Online
         }
         private void log_Click(object sender, EventArgs e)
         {
-            RecordUndoEvent("KukaTargets");
+            RecordUndoEvent("Log");
             logOption = !logOption;
-            //ExpireSolution(true);
+
+            if (logOption)
+            {
+                AddOutput(0);
+            }
+            else
+            {
+                Params.UnregisterOutputParameter(Params.Output.FirstOrDefault(x => x.Name == "Log"), true);
+            }
+
+            ExpireSolution(true);
         }
+    
 
         // Register the new input parameters to our component.
         private void AddInput(int index)
@@ -330,6 +198,7 @@ namespace Axis.Online
                 Params.RegisterOutputParam(parameter, insertIndex);
             }
             Params.OnParametersChanged();
+
             ExpireSolution(true);
         }
 
