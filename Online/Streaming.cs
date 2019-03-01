b07c0b19f003756867740eb66e558fa3ad616881
Povl Filip Sonne-Frederiksen <psf@axisarch.tech>
2019-03-01 16:01:22 +0100

    Black Start/Stop & Streaming component

---
+++ Online/Streaming.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Parameters;
+
+
+using Rhino.Geometry;
+
+using ABB.Robotics;
+using ABB.Robotics.Controllers;
+using ABB.Robotics.Controllers.RapidDomain;
+using ABB.Robotics.Controllers.Discovery;
+using ABB.Robotics.Controllers.Messaging;
+using ABB.Robotics.Controllers.IOSystemDomain;
+
+using Axis.Targets;
+
+namespace Axis.Online
+{
+    public class Streaming : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public Streaming()
+          : base("Live Strem Code", "Strem",
+              "Stream instructions to a robot controller",
+              "Axis", "9. Online")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return Properties.Resources.Online;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("b2f9edfc-14d6-4db0-a569-6d3a0ddec76a"); }
+        }
+    }
+}
