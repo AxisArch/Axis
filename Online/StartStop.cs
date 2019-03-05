c6cc95fdffedc86c952389e8657ee9260d56acba
Povl Filip Sonne-Frederiksen <psf@axisarch.tech>
2019-03-05 16:23:47 +0100

    Implemented Start/Stop and Streaming component

--- Online/StartStop.cs
+++ Online/StartStop.cs
@@ -24,8 +24,17 @@ using Axis.Targets;
 
 namespace Axis.Online
 {
-    public class StartStop : GH_Component
+    public class StartStop : GH_Component, IGH_VariableParameterComponent
     {
+        //Optionable Log
+        private bool logOption = false;
+        private bool logOptionOut = false;
+        private List<string> log = new List<string>();
+        public List<string> Status { get; set; }
+
+        public Controller controllers = null;
+        private Task[] tasks = null;
+
         /// <summary>
         /// Initializes a new instance of the MyComponent1 class.
         /// </summary>
@@ -41,6 +50,10 @@ namespace Axis.Online
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddGenericParameter("Controller", "Controller", "Recives the output from a controller module", GH_ParamAccess.list);
+            pManager.AddBooleanParameter("Reset PP", "Reset PP", "Set the program pointed back to the main entry point for the current task.", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("Begin", "Begin", "Start the default task on the controller.", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("Stop", "Stop", "Stop the default task on the controller.", GH_ParamAccess.item, false);
         }
 
         /// <summary>
@@ -56,7 +69,242 @@ namespace Axis.Online
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            List<GH_ObjectWrapper> controllers = new List<GH_ObjectWrapper>();
+            Controller abbController = null;
+            bool resetPP = false;
+            bool begin = false;
+            bool stop = false;
+            bool clear = false;
+            if (!DA.GetDataList("Controller", controllers)) ;
+            if (!DA.GetData("Reset PP", ref resetPP)) ;
+            if (!DA.GetData("Begin", ref begin)) ;
+            if (!DA.GetData("Stop", ref stop)) ;
+            if (logOption)
+            {
+                if (!DA.GetData("Clear", ref clear)) ;
+            }
+
+
+            for (int i = 0; i < controllers.Count; i++)
+            {
+                //Check for valid input, else top execuion
+                AxisController myAxisController = controllers[i].Value as AxisController;
+                if ((myAxisController != null) && (myAxisController.axisControllerState == true))
+                {
+                    abbController = myAxisController;
+                }
+                else { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "No active controller connected"); return; }
+
+                tasks = abbController.Rapid.GetTasks();
+
+                if (resetPP && abbController != null)
+                {
+                    using (Mastership m = Mastership.Request(abbController.Rapid))
+                    {
+                        // Reset program pointer to main.
+                        try
+                        {
+                            tasks[0].ResetProgramPointer();                            
+                        }
+                        catch (Exception){log.Add("Opperation not allowed in current state");}
+
+                    }
+                }
+
+                if (begin)
+                {
+                    // Execute robot tasks present on controller.
+                    try
+                    {
+                        if (abbController.OperatingMode == ControllerOperatingMode.Auto)
+                        {
+                            using (Mastership m = Mastership.Request(abbController.Rapid))
+                            {
+                                // Perform operation.
+                                tasks[0].Start();
+                                log.Add("Robot program started on robot " + abbController.SystemName + ".");
+                            }
+                        }
+                        else
+                        {
+                            log.Add("Automatic mode is required to start execution from a remote client.");
+                        }
+                    }
+                    catch (System.InvalidOperationException ex)
+                    {
+                        log.Add("Mastership is held by another client." + ex.Message);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        log.Add("Unexpected error occurred: " + ex.Message);
+                    }
+                }
+
+                if (stop)
+                {
+                    try
+                    {
+                        if (abbController.OperatingMode == ControllerOperatingMode.Auto)
+                        {
+                            using (Mastership m = Mastership.Request(abbController.Rapid))
+                            {
+                                // Stop operation.
+                                tasks[0].Stop(StopMode.Immediate);
+                                log.Add("Robot program stopped on robot " + abbController.SystemName + ".");
+                            }
+                        }
+                        else
+                        {
+                            log.Add("Automatic mode is required to stop execution from a remote client.");
+                        }
+                    }
+                    catch (System.InvalidOperationException ex)
+                    {
+                        log.Add("Mastership is held by another client." + ex.Message);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        log.Add("Unexpected error occurred: " + ex.Message);
+                    }
+                }
+            }
+
+            // Clear log
+            if (clear)
+            {
+                log.Clear();
+                log.Add("Log cleared.");
+            }
+
+            //Output log
+            if (logOptionOut)
+            {
+                Status = log;
+                DA.SetDataList("Log", log);
+            }
         }
+
+        /// <summary>
+        /// Additional Input and Output parameters for the component
+        /// </summary>
+        // Build a list of optional input parameters
+        IGH_Param[] inputParams = new IGH_Param[1]
+        {
+            new Param_Boolean() { Name = "Clear", NickName = "Clear", Description = "Clear the communication log.", Access = GH_ParamAccess.item, Optional = true},
+        };
+        // Build a list of optional output parameters
+        IGH_Param[] outputParams = new IGH_Param[1]
+        {
+        new Param_String() { Name = "Log", NickName = "Log", Description = "Log checking the connection status"},
+        };
+
+        // The following functions append menu items and then handle the item clicked event.
+        protected override void AppendAdditionalComponentMenuItems(System.Windows.Forms.ToolStripDropDown menu)
+        {
+            ToolStripMenuItem log = Menu_AppendItem(menu, "Log", log_Click, true, logOption);
+            log.ToolTipText = "Activate the log output";
+
+            //ToolStripSeparator seperator = Menu_AppendSeparator(menu);
+        }
+        private void log_Click(object sender, EventArgs e)
+        {
+            RecordUndoEvent("Log");
+            logOption = !logOption;
+
+            if (logOption)
+            {
+                AddInput(0);
+                AddOutput(0);
+                logOptionOut = true;
+            }
+            else
+            {
+                Params.UnregisterInputParameter(Params.Input.FirstOrDefault(x => x.Name == "Clear"), true);
+                Params.UnregisterOutputParameter(Params.Output.FirstOrDefault(x => x.Name == "Log"), true);
+                logOptionOut = false;
+            }
+
+            //ExpireSolution(true);
+        }
+
+
+        // Register the new input parameters to our component.
+        private void AddInput(int index)
+        {
+            IGH_Param parameter = inputParams[index];
+
+            if (Params.Input.Any(x => x.Name == parameter.Name))
+                Params.UnregisterInputParameter(Params.Input.First(x => x.Name == parameter.Name), true);
+            else
+            {
+                int insertIndex = Params.Input.Count;
+                for (int i = 0; i < Params.Input.Count; i++)
+                {
+                    int otherIndex = Array.FindIndex(inputParams, x => x.Name == Params.Input[i].Name);
+                    if (otherIndex > index)
+                    {
+                        insertIndex = i;
+                        break;
+                    }
+                }
+
+                Params.RegisterInputParam(parameter, insertIndex);
+            }
+            Params.OnParametersChanged();
+            ExpireSolution(true);
+        }
+        // Register the new output parameters to our component.
+        private void AddOutput(int index)
+        {
+            IGH_Param parameter = outputParams[index];
+
+            if (Params.Output.Any(x => x.Name == parameter.Name))
+                Params.UnregisterOutputParameter(Params.Output.First(x => x.Name == parameter.Name), true);
+            else
+            {
+                int insertIndex = Params.Output.Count;
+                for (int i = 0; i < Params.Output.Count; i++)
+                {
+                    int otherIndex = Array.FindIndex(outputParams, x => x.Name == Params.Output[i].Name);
+                    if (otherIndex > index)
+                    {
+                        insertIndex = i;
+                        break;
+                    }
+                }
+
+                Params.RegisterOutputParam(parameter, insertIndex);
+            }
+            Params.OnParametersChanged();
+
+            ExpireSolution(true);
+        }
+
+        // Serialize this instance to a Grasshopper writer object.
+        public override bool Write(GH_IO.Serialization.GH_IWriter writer)
+        {
+            writer.SetBoolean("LogOptionSetModule", this.logOption);
+            writer.SetBoolean("LogOptionSetOutModule", this.logOptionOut);
+            return base.Write(writer);
+        }
+
+        // Deserialize this instance from a Grasshopper reader object.
+        public override bool Read(GH_IO.Serialization.GH_IReader reader)
+        {
+            this.logOption = reader.GetBoolean("LogOptionSetModule");
+            this.logOptionOut = reader.GetBoolean("LogOptionSetOutModule");
+            return base.Read(reader);
+        }
+
+        /// <summary>
+        /// Implement this interface in your component if you want to enable variable parameter UI.
+        /// </summary>
+        bool IGH_VariableParameterComponent.CanInsertParameter(GH_ParameterSide side, int index) => false;
+        bool IGH_VariableParameterComponent.CanRemoveParameter(GH_ParameterSide side, int index) => false;
+        IGH_Param IGH_VariableParameterComponent.CreateParameter(GH_ParameterSide side, int index) => null;
+        bool IGH_VariableParameterComponent.DestroyParameter(GH_ParameterSide side, int index) => false;
+        void IGH_VariableParameterComponent.VariableParameterMaintenance() { }
+
 
         /// <summary>
         /// Provides an Icon for the component.
--- Online/Streaming.cs
+++ Online/Streaming.cs
@@ -24,8 +24,37 @@ using Axis.Targets;
 
 namespace Axis.Online
 {
-    public class Streaming : GH_Component
+    public class Streaming : GH_Component, IGH_VariableParameterComponent
     {
+        //Optionable Log
+        private bool logOption = false;
+        private bool logOptionOut = false;
+        public List<string> Status { get; set; }
+
+        public Controller controller = null;
+        private Task[] tasks = null;
+        public IpcQueue RobotQueue { get; set; }
+
+        private bool stream;
+        private int ioMonitoringOn = 0;
+        private int ioMonitoringOff = 0;
+        private int tcpMonitoringOn = 0;
+        private int tcpMonitoringOff = 0;
+
+
+        ABB.Robotics.Controllers.RapidDomain.RobTarget cRobTarg;
+        System.Byte[] data;
+
+        // Create a list of string to store a log of the connection status.
+        private List<string> log = new List<string>();
+        private List<string> IOstatus = new List<string>();
+        private Plane tcp = new Plane();
+
+        Pos pos = new Pos();
+        Orient ori = new Orient();
+        Pose pose = new Pose();
+
+
         /// <summary>
         /// Initializes a new instance of the MyComponent1 class.
         /// </summary>
@@ -41,6 +70,14 @@ namespace Axis.Online
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddGenericParameter("Controller", "Controller", "Recives the output from a controller module", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Stream", "Stream", "Begin streaming to the robot.", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("TCP", "TCP", "Opional monitoring of the TCP.", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("IO", "IO", "Opional monitoring of the IO system. (Only signals registered as common will be monitored.)", GH_ParamAccess.item, false);
+            pManager.AddGenericParameter("Target", "Target", "Target for robot positioning.", GH_ParamAccess.item);
+
+            //Input optional
+            for (int i = 0; i < 5; ++i){pManager[i].Optional = true;}
         }
 
         /// <summary>
@@ -48,6 +85,8 @@ namespace Axis.Online
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddTextParameter("IO", "IO", "IO status.", GH_ParamAccess.list);
+            pManager.AddPlaneParameter("TCP", "TCP", "TCP status.", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -56,7 +95,313 @@ namespace Axis.Online
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            GH_ObjectWrapper controller = new GH_ObjectWrapper();
+            Controller abbController = null;
+            bool clear = false;
+            bool monitorTCP = false;
+            bool monitorIO = false;
+            bool stream = false;
+            Target targ = null;
+
+            if (!DA.GetData("Controller", ref controller)) ;
+            if (!DA.GetData("Stream", ref stream)) ;
+            if (!DA.GetData("TCP", ref monitorTCP)) ;
+            if (!DA.GetData("IO", ref monitorIO)) ;
+            if (!DA.GetData("Target", ref targ)) ;
+            if (logOption)
+            {
+                if (!DA.GetData("Clear", ref clear)) ;
+            }
+
+
+            double cRobX = 0;
+            double cRobY = 0;
+            double cRobZ = 0;
+
+            double cRobQ1 = 0;
+            double cRobQ2 = 0;
+            double cRobQ3 = 0;
+            double cRobQ4 = 0;
+
+            Quaternion cRobQuat = new Quaternion();
+
+            //Check for valid input, else top execuion
+            AxisController myAxisController = controller.Value as AxisController;
+            if ((myAxisController != null) && (myAxisController.axisControllerState == true))
+            {
+                abbController = myAxisController;
+            }
+            else { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "No active controller connected"); return; }
+
+            // Get T_ROB1 queue to send messages to the RAPID task.
+            if (!abbController.Ipc.Exists("RMQ_T_ROB1"))
+            {
+                abbController.Ipc.CreateQueue("RMQ_T_ROB1", 10, Ipc.MaxMessageSize);
+            }
+
+            // Get RobotQueue
+            if (RobotQueue== null)
+            {
+                tasks = abbController.Rapid.GetTasks();
+                IpcQueue robotQueue = abbController.Ipc.GetQueue("RMQ_T_ROB1");
+                int queueID = robotQueue.QueueId;
+                string queueName = robotQueue.QueueName;
+
+                log.Add("Established RMQ for T_ROB1 on network controller.");
+                log.Add("Rapid Message Queue ID:" + queueID.ToString() + ".");
+                log.Add("Rapid Message Queue Name:" + queueName + ".");
+                RobotQueue = robotQueue;
+            }
+
+            // If active, update the status of the TCP.
+            if (monitorTCP)
+            {
+                if (tcpMonitoringOn == 0)
+                {
+                    log.Add("TCP monitoring started.");
+                }
+
+                cRobTarg = tasks[0].GetRobTarget();
+
+                cRobX = Math.Round(cRobTarg.Trans.X, 3);
+                cRobY = Math.Round(cRobTarg.Trans.Y, 3);
+                cRobZ = Math.Round(cRobTarg.Trans.Z, 3);
+
+                Point3d cRobPos = new Point3d(cRobX, cRobY, cRobZ);
+
+                cRobQ1 = Math.Round(cRobTarg.Rot.Q1, 5);
+                cRobQ2 = Math.Round(cRobTarg.Rot.Q2, 5);
+                cRobQ3 = Math.Round(cRobTarg.Rot.Q3, 5);
+                cRobQ4 = Math.Round(cRobTarg.Rot.Q4, 5);
+
+                cRobQuat = new Quaternion(cRobQ1, cRobQ2, cRobQ3, cRobQ4);
+
+                tcp = Util.QuaternionToPlane(cRobPos, cRobQuat);
+
+                tcpMonitoringOn += 1;
+                tcpMonitoringOff = 0;
+            }
+            else if (tcpMonitoringOn > 0)
+            {
+                if (tcpMonitoringOff == 0)
+                {
+                    log.Add("TCP monitoring stopped.");
+                }
+
+                tcpMonitoringOff += 1;
+                tcpMonitoringOn = 0;
+            }
+
+            // If active, update the status of the IO system.
+            if (monitorIO)
+            {
+                if (ioMonitoringOn == 0)
+                {
+                    log.Add("Signal monitoring started.");
+                }
+
+                // Filter only the digital IO system signals.
+                IOFilterTypes dSignalFilter = IOFilterTypes.Common;
+                SignalCollection dSignals = abbController.IOSystem.GetSignals(dSignalFilter);
+
+                IOstatus.Clear();
+                // Iterate through the found collection and print them to the IO monitoring list.
+                foreach (Signal signal in dSignals)
+                {
+                    string sigVal = signal.ToString() + ": " + signal.Value.ToString();
+                    IOstatus.Add(sigVal);
+                }
+
+                ioMonitoringOn += 1;
+                ioMonitoringOff = 0;
+            }
+            else if (ioMonitoringOn > 0)
+            {
+                if (ioMonitoringOff == 0)
+                {
+                    log.Add("Signal monitoring stopped.");
+                }
+
+                ioMonitoringOff += 1;
+                ioMonitoringOn = 0;
+            }
+
+            // Steam a target to a controller
+            if (stream)
+            {
+                if (targ != null)
+                {
+                    IpcMessage message = new IpcMessage();
+
+                    pos.X = (float)targ.Position.X;
+                    pos.Y = (float)targ.Position.Y;
+                    pos.Z = (float)targ.Position.Z;
+
+
+                    ori.Q1 = targ.Quaternion.A;
+                    ori.Q2 = targ.Quaternion.B;
+                    ori.Q3 = targ.Quaternion.C;
+                    ori.Q4 = targ.Quaternion.D;
+
+                    pose.Trans = pos;
+                    pose.Rot = ori;
+
+                    string content = "SD;[" +
+                        "\"Linear\"," +
+                        pos.ToString() + "," +
+                        ori.ToString() + "," +
+                        pose.ToString() +
+                        "]";
+
+                    byte[] data = new UTF8Encoding().GetBytes(content);
+
+                    message.SetData(data);
+                    RobotQueue.Send(message);
+                }
+            }
+
+            // Clear log
+            if (clear)
+            {
+                log.Clear();
+                log.Add("Log cleared.");
+            }
+
+            //Output log
+            if (logOptionOut)
+            {
+                Status = log;
+                DA.SetDataList("Log", log);
+            }
+
+            // Output IO & TCP
+            DA.SetDataList("IO", IOstatus);
+            DA.SetData("TCP", new GH_Plane(tcp));
+
+            ExpireSolution(true);
+
         }
+
+        /// <summary>
+        /// Additional Input and Output parameters for the component
+        /// </summary>
+        // Build a list of optional input parameters
+        IGH_Param[] inputParams = new IGH_Param[1]
+        {
+            new Param_Boolean() { Name = "Clear", NickName = "Clear", Description = "Clear the communication log.", Access = GH_ParamAccess.item, Optional = true},
+        };
+        // Build a list of optional output parameters
+        IGH_Param[] outputParams = new IGH_Param[1]
+        {
+        new Param_String() { Name = "Log", NickName = "Log", Description = "Log checking the connection status"},
+        };
+
+        // The following functions append menu items and then handle the item clicked event.
+        protected override void AppendAdditionalComponentMenuItems(System.Windows.Forms.ToolStripDropDown menu)
+        {
+            ToolStripMenuItem log = Menu_AppendItem(menu, "Log", log_Click, true, logOption);
+            log.ToolTipText = "Activate the log output";
+
+            //ToolStripSeparator seperator = Menu_AppendSeparator(menu);
+        }
+        private void log_Click(object sender, EventArgs e)
+        {
+            RecordUndoEvent("Log");
+            logOption = !logOption;
+
+            if (logOption)
+            {
+                AddInput(0);
+                AddOutput(0);
+                logOptionOut = true;
+            }
+            else
+            {
+                Params.UnregisterInputParameter(Params.Input.FirstOrDefault(x => x.Name == "Clear"), true);
+                Params.UnregisterOutputParameter(Params.Output.FirstOrDefault(x => x.Name == "Log"), true);
+                logOptionOut = false;
+            }
+
+            //ExpireSolution(true);
+        }
+
+
+        // Register the new input parameters to our component.
+        private void AddInput(int index)
+        {
+            IGH_Param parameter = inputParams[index];
+
+            if (Params.Input.Any(x => x.Name == parameter.Name))
+                Params.UnregisterInputParameter(Params.Input.First(x => x.Name == parameter.Name), true);
+            else
+            {
+                int insertIndex = Params.Input.Count;
+                for (int i = 0; i < Params.Input.Count; i++)
+                {
+                    int otherIndex = Array.FindIndex(inputParams, x => x.Name == Params.Input[i].Name);
+                    if (otherIndex > index)
+                    {
+                        insertIndex = i;
+                        break;
+                    }
+                }
+
+                Params.RegisterInputParam(parameter, insertIndex);
+            }
+            Params.OnParametersChanged();
+            ExpireSolution(true);
+        }
+        // Register the new output parameters to our component.
+        private void AddOutput(int index)
+        {
+            IGH_Param parameter = outputParams[index];
+
+            if (Params.Output.Any(x => x.Name == parameter.Name))
+                Params.UnregisterOutputParameter(Params.Output.First(x => x.Name == parameter.Name), true);
+            else
+            {
+                int insertIndex = Params.Output.Count;
+                for (int i = 0; i < Params.Output.Count; i++)
+                {
+                    int otherIndex = Array.FindIndex(outputParams, x => x.Name == Params.Output[i].Name);
+                    if (otherIndex > index)
+                    {
+                        insertIndex = i;
+                        break;
+                    }
+                }
+
+                Params.RegisterOutputParam(parameter, insertIndex);
+            }
+            Params.OnParametersChanged();
+
+            ExpireSolution(true);
+        }
+
+        // Serialize this instance to a Grasshopper writer object.
+        public override bool Write(GH_IO.Serialization.GH_IWriter writer)
+        {
+            writer.SetBoolean("LogOptionSetModule", this.logOption);
+            writer.SetBoolean("LogOptionSetOutModule", this.logOptionOut);
+            return base.Write(writer);
+        }
+
+        // Deserialize this instance from a Grasshopper reader object.
+        public override bool Read(GH_IO.Serialization.GH_IReader reader)
+        {
+            this.logOption = reader.GetBoolean("LogOptionSetModule");
+            this.logOptionOut = reader.GetBoolean("LogOptionSetOutModule");
+            return base.Read(reader);
+        }
+
+        /// <summary>
+        /// Implement this interface in your component if you want to enable variable parameter UI.
+        /// </summary>
+        bool IGH_VariableParameterComponent.CanInsertParameter(GH_ParameterSide side, int index) => false;
+        bool IGH_VariableParameterComponent.CanRemoveParameter(GH_ParameterSide side, int index) => false;
+        IGH_Param IGH_VariableParameterComponent.CreateParameter(GH_ParameterSide side, int index) => null;
+        bool IGH_VariableParameterComponent.DestroyParameter(GH_ParameterSide side, int index) => false;
+        void IGH_VariableParameterComponent.VariableParameterMaintenance() { }
 
         /// <summary>
         /// Provides an Icon for the component.
