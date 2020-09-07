c0f136ca3d60ee913ccfd283f4dc4af455065411
Povl Filip Sonne-Frederiksen <psf@aarch.dk>
2020-09-07 19:35:23 +0200

    Removing MoreLinq

--- Plugin/Targets/CreateTarget_multithreaded.cs
+++ Plugin/Targets/CreateTarget_multithreaded.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Windows.Forms;
 using System.Threading.Tasks;
 
-using MoreLinq;
+//using MoreLinq;
 
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Parameters;
